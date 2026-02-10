c0e5cafa77de276994d320ec123ce40faae166a8
Manuel A. Martinez-Delgado <manuelmrtzd@gmail.com>
2026-02-09 23:29:08 -0400

    Modified comment area

--- ch01a07/ch01a07.cbl
+++ ch01a07/ch01a07.cbl
@@ -4,15 +4,16 @@
       *AUTHOR. MANUEL A. MARTINEZ.
       *DATE-WRITTEN. 02-09-2026.
       *REMARKS.
-      *    Speeds used in aeronautical or maritime application are 
-      *    ussualy given in knots, or nauticale miles per hour, rather
-      *    than in statute miles per hour, which is the commonly used 
-      *    unit on land. Code an interactive program in its entirely
-      *    that will accept a value in knots, convert it to miles per
-      *    hour, and then display the converted value (1 knot = 1.15
-      *    statute miles per hour).
-              
-       
+      ******************************************************************
+      *    Speeds used in aeronautical or maritime application are     *
+      *    ussualy given in knots, or nauticale miles per hour, rather *
+      *    than in statute miles per hour, which is the commonly used  *
+      *    unit on land. Code an interactive program in its entirely   *
+      *    that will accept a value in knots, convert it to miles per  *
+      *    hour, and then display the converted value (1 knot = 1.15   *
+      *    statute miles per hour).                                    *
+      ******************************************************************
+
        ENVIRONMENT DIVISION.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
